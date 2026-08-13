a2383500c3dee1862b7430bb20e59d7187acbf28
Maddipatla Chatan <chetan121318@gmail.com>
2026-08-13 21:10:58 +0530

    Update face_eye_simle.py

--- face_eye_simle.py
+++ face_eye_simle.py
@@ -6,33 +6,6 @@ smile_cascade = cv2.CascadeClassifier("/Users/chetan/Documents/face emotion dect
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    ret,frame = cap.read()
-    gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray,1.1,5)
-
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
-    roi_gray = gray[y:y+h,x:x+w]
-    # detect smile and eyes
-    roi_color = frame[y:y+h,x:x+w]
-    # text or boxes gets drawn
-
-    eyes = eye_cascade.detectMultiScale(gray,1.1,10)
-
-    if len(eyes) >0:
-        cv2.put(frame,"Eyes Detected",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,(0,0,255),2)
-
-    smile = smile_cascade.detectMultiScale(gray,1.7,20)
-
-    if len(smile) >0:
-        cv2.put(frame,"Smile Detected",(x,y-10),cv2.FONT_HERSHEY_COMPLEX,(0,0,255),2)
-    cv2.imshow("smart face dection ",frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
 cv2.destroyAllWindows()
 
      
