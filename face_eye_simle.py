855e30bba7698ce7a6f722c9b15df0babc561285
Maddipatla Chatan <chetan121318@gmail.com>
2026-08-13 21:11:08 +0530

    Update face_eye_simle.py

--- face_eye_simle.py
+++ face_eye_simle.py
@@ -1,8 +1,6 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier("/Users/chetan/Documents/face emotion dection/haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier("/Users/chetan/Documents/face emotion dection/haarcascade_eye.xml")
-smile_cascade = cv2.CascadeClassifier("/Users/chetan/Documents/face emotion dection/haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier("/Users/chetanontalface_default.xml")
 
 cap = cv2.VideoCapture(0)
 
